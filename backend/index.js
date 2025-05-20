e1d07958d7f4517a07524ead28d207049e1dea55
Programador Mobile <programador@sfiec.org.br>
2025-05-20 20:59:47 -0300

    segunda atz 20/05

--- backend/index.js
+++ backend/index.js
@@ -24,12 +24,35 @@
 
 const express = require('express');
 const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
 // Configuração de middleware
 app.use(cors());
 app.use(express.json()); // Substitui body-parser para JSON
+
+const db = new sqlite3.Database('.tarefas.db',(err) => 
+{
+    if (err) {
+        console.error("Erro ao conectar ao Sqlite: ",err);
+
+    } else {
+        console.error("Conectado ao Sqlite: ")
+    }
+}
+);
+
+
+db.serialize(() => {
+    db.run(
+        'CREATE TABLE IF NOT EXIXSTS usuarios(id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT UNIQUE NOT NULL, password TEXT NOT NULL)'
+    );
+    db.run(
+        'CREATE TABLE IF NOT EXIXSTS tarefas(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT NOT NULL, userId INTEGER NOT NULL, FOREIGN KEY (userId) REFERENCES usuarios(id))'
+    );
+});
+
 app.use(express.urlencoded({ extended: true })); // Para dados de formulário codificados na URL
 
 // Definição da rota de teste
@@ -42,4 +65,16 @@ const PORT = 3001;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}, acesse http://localhost:${PORT}`);
+});
+
+
+process.on('SIGINT', () => {
+    db.close((err) => {
+        if (err) {
+            console.error('Erro ao fechar o sqlite', err);
+        } else {
+            console.log('Sqlite fechado com sucesso')
+            process.exit(0);
+        }
+    });
 });
